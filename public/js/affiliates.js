2677c6c2419e89e219435eb4afdc0cbe70ae0de0
raymondchen86@hotmail.com <raymondchen86@hotmail.com>
2018-03-25 01:14:48 -0700

    add images, media queries and links

---
+++ public/js/affiliates.js
@@ -0,0 +1,22 @@
+let affiliates = [
+  {
+  name: "Trilogy Systems Limited",
+  link: "www.trilogy-systems.net",
+
+}
+]
+
+
+// $("#insert_card").append(
+// `<div class="card col-md-4 card_input">
+//   <img class="card-img-top" src="${data.val().imageURL}">
+//   <div class="card-block"> ${data.val().caption} </div>
+// </div>`
+// )
+
+$(document).ready(function() {
+  for(let i=0; i<affiliates.length; i++){
+
+  }
+
+}
